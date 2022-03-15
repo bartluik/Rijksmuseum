276dd33cb440dbe1d4d298d4446eb2de02623a43
Bart Luik <bart.luik@persgroep.net>
2022-03-15 21:54:59 +0100

    Remove unused binding reference from art activity

--- art/ui/src/main/kotlin/it/luik/rijksmuseum/ui/art/ArtActivity.kt
+++ art/ui/src/main/kotlin/it/luik/rijksmuseum/ui/art/ArtActivity.kt
@@ -8,11 +8,8 @@ import it.luik.rijksmuseum.ui.databinding.ActivityArtBinding
 @AndroidEntryPoint
 internal class ArtActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityArtBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityArtBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(ActivityArtBinding.inflate(layoutInflater).root)
     }
 }
